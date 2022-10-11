694c4b5c5ad978efd9a9afd43517216a0777db17
Konstantin Hulek <chaoticdragon28@gmail.com>
2022-10-11 19:31:53 +0200

    Work in progress this currently doesnt work!!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from re import M
+from tracemalloc import start
 title = "  _    _                                           __   ___  \n | |  | |                                         /_ | / _ \ \n | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __    | || | | |\n |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \   | || | | |\n | |  | | (_| | | | | (_| | | | | | | (_| | | | |  | || |_| |\n |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  |_(_)___/ \n                      __/ |                                  \n                     |___/                                   \n" #the hangman title
 def get_word(): #load world list
      with open('words/DE_all.txt') as f:
@@ -23,15 +24,39 @@ def gen_hangman(mistakes): #generate the hangman image for any given mistakes
         return " ┌───┐     \n │   O     \n │  /│\    \n │  / \    \n │         \n┌┴───────┐ \n│        └┐\n└─────────┘\n"
     else:
         return 0
-print(title)
+
+input = ""
 word = get_word()
-for i in range(len(word)-1):
-    print("_" + str(i)+ " ", end="")
-print('')
-print(word)
-input = input("Enter a leter: ")
-if(input.upper() in (word.upper())):
-    yes = "Correct"
-    print(yes)
-#def function1():
-#
+mistakes = 0
+guessed_letters = ""
+def print_game():
+    #prints title, hangman statue and filld letters
+    print(title)
+    print(gen_hangman(mistakes))
+    for i in range(len(word)-1):
+        if(word[i] in guessed_letters):
+            print(word[i]+" ", end="")
+        else:
+            print("_ ", end="")
+    print('')
+    print(word)
+    input = input("Enter a leter: ")
+print_game()
+
+if(input.upper() in word.upper()):
+     #if letter correct
+    guessed_letters+=input
+
+
+
+elif(mistakes==6):
+    print(gen_hangman(mistakes))
+    print("Game Over!")
+    print("The word was: " + word)
+    #end game
+else:
+    #wrong letter entered(no death)
+    mistakes+=1
+
+
+
